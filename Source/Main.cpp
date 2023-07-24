c7fc9eac22519f474f8cc534bda2cd0c17a5b972
Bernardo Escalona <bernardo.escalona@dbaudio.com>
2023-07-24 21:21:10 +0200

    Fixed some warnings, set resize limits

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -39,32 +39,23 @@ public:
     const juce::String getApplicationVersion() override    { return ProjectInfo::versionString; }
     bool moreThanOneInstanceAllowed() override             { return true; }
 
-    void initialise (const juce::String& commandLine) override
+    void initialise (const juce::String& /*commandLine*/) override
     {
-        // This method is where you should put your application's initialisation code..
-
         m_mainWindow.reset (new MainWindow (getApplicationName()));
     }
 
     void shutdown() override
     {
-        // Add your application's shutdown code here..
-
-        m_mainWindow = nullptr; // (deletes our window)
+        m_mainWindow = nullptr;
     }
 
     void systemRequestedQuit() override
     {
-        // This is called when the app is being asked to quit: you can ignore this
-        // request and let the app carry on running, or call quit() to allow the app to close.
         quit();
     }
 
-    void anotherInstanceStarted (const juce::String& commandLine) override
+    void anotherInstanceStarted (const juce::String& /*commandLine*/) override
     {
-        // When another instance of the app is launched while this one is running,
-        // this method is invoked, and the commandLine parameter tells you what
-        // the other instance's command-line arguments were.
     }
 
     /*
@@ -87,6 +78,7 @@ public:
             setFullScreen (true);
            #else
             setResizable (true, true);
+            setResizeLimits(getWidth(), getHeight(), 3840, 2160);
             centreWithSize (getWidth(), getHeight());
            #endif
 
