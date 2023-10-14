a0e141fbe614e346efd235810ba6c8deac2fe08d
Christian Ahrens <christianahrens@me.com>
2023-10-14 12:42:58 +0200

    Added macOS DiskImage creation utils to Resources/Deployment/macOS and submodule create-dmg

---
+++ Resources/Deployment/macOS/create_installer.sh
@@ -0,0 +1,31 @@
+# we are in Resources/Deployment/macOS/ -> change directory to project root
+cd ../../../
+
+# set external variables
+CodeSignCertName=$1 # e.g. "Developer ID Application: SomeIdentifier"
+NotarizationUser=$2 # appleid to use for notarization
+AppBundleId=$3 # app bundle id like "com.SomeIdentifier.AES70_OCP1_StringGenerator"
+
+echo "Using certificate $CodeSignCertName for codesigning."
+echo "Using AppleId $NotarizationUser and bundle name $AppBundleId for notarization."
+
+# set convenience variables
+AppBundlePath=Builds/MacOSX/build/Release
+CreateDmgPath=submodules/create-dmg/create-dmg
+DmgTargetPath=AES70_OCP1_StringGenerator.dmg
+
+# build the project and create dmg
+cd Resources/Deployment/macOS
+./build_project.sh
+./create_diskimage.sh
+cd ../../../
+
+# codesign the disk image
+codesign --force --sign "$CodesignCertName" "$DmgTargetPath"
+
+# trigger notarization
+# (assumes that a keychain item "SCB_DMG_Notarization_PW" exists, created e.g. with "xcrun altool --store-password-in-keychain-item "SCB_DMGNotarization_PW" -u "abc" -p "def"")
+xcrun altool --notarize-app --primary-bundle-id "$AppBundleId" --username "$NotarizationUser" --password "@keychain:SCB_DMGNotarization_PW" --file "$DmgTargetPath"
+
+# notarization result can be queried as follows
+# xcrun altool --notarization-info 2EFE2717-52EF-43A5-96DC-0797E4CA1041 -u "$NotarizationUser"
